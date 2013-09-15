01822b4ac86a0178c62fdebf7e8436868269bc18
Meeka <rrgayhart@gmail.com>
2013-09-14 18:32:56 -0600

    update for challenge problem

--- ex33.rb
+++ ex33.rb
@@ -19,4 +19,23 @@ class Looper
 		puts num
 		end
 	end
+	
+	def while_loop_for(i, max, incr)
+		numbers = []
+		
+		for i in (i..max)
+			puts "At the top i is #{i}"
+			numbers.push(i)
+			
+			i += incr
+			puts "Numbers now: #{numbers}"
+			puts "At the bottom i is #{i}"
+		end
+		
+		puts "The numbers: "
+		
+		for num in numbers
+			puts num
+		end
+	end
 end
