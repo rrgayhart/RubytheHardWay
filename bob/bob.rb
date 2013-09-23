741f58202b1ea19065970c796474f0e6ae918e5a
Meeka <rrgayhart@gmail.com>
2013-09-23 12:31:34 -0600

    completes ex40

---
+++ bob/bob.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+
+class Bob
+
+  def hey(input)
+    if input == input.upcase
+      return "Woah, chill out!"
+    elsif input == ""
+      return "Fine. Be that way!"
+    elsif input.ends_with?()
+      return "Sure."
+    else
+    return "Whatever."
+    end
+  end
+
+end
---
+++ ex40.rb
@@ -0,0 +1,25 @@
+class Song
+
+    def initialize(lyrics)
+        @lyrics = lyrics
+    end
+
+    def sing_me_a_song()
+        for line in @lyrics
+            puts line
+        end
+    end
+end
+
+happy_bday = Song.new(["Happy birthday to you",
+                   "I don't want to get sued",
+                   "So I'll stop right there"])
+
+lyrics = ["They rally around the family",
+                        "With pockets full of shells"]
+
+bulls_on_parade = Song.new(lyrics)
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
