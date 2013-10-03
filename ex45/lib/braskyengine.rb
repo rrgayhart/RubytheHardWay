f84060f55312983a7bbb5c492841fbb3521d1443
Meeka <rrgayhart@gmail.com>
2013-10-03 15:15:18 -0600

    sets up framework for ex45

---
+++ ex45/lib/braskyengine.rb
@@ -0,0 +1,3 @@
+class BraskyEngine
+
+end
---
+++ ex45/test/brasky_engine_test.rb
@@ -0,0 +1,8 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/braskyengine'
+
+class BraskyEngineTest < MiniTest::Test
+
+end
